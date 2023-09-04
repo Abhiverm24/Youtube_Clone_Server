46a54a0ca6175299868620224756b4b8af125d68
Abhishek24 <abhishekv2403@gmail.com>
2023-09-05 00:01:02 +0530

    commit

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {login} from '../controller/auth.js'
+import {login} from '../controller/auth.js';
 import {updateChannelData,getAllChannels} from '../controller/channel.js'
 
 const routes = express.Router();
